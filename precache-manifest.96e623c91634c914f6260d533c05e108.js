79ab56cb07bb2cba1f0f63546f426354d64c86d5
MohammadAli <mh.newday@gmail.com>
2020-07-28 12:58:31 +0430

    Updates

---
+++ precache-manifest.96e623c91634c914f6260d533c05e108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58329e9bf6199459a4528668c1ba6dc0",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "dfc0e8d71179403f87c4",
+    "url": "/resume/static/css/main.bf4e409d.chunk.css"
+  },
+  {
+    "revision": "c04cfb2bfcb0e2db3a2a",
+    "url": "/resume/static/js/2.c723e2a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resume/static/js/2.c723e2a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfc0e8d71179403f87c4",
+    "url": "/resume/static/js/main.06b8454b.chunk.js"
+  },
+  {
+    "revision": "1693372dd29571915f2b",
+    "url": "/resume/static/js/runtime-main.368e0faf.js"
+  }
+]);
